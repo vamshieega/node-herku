18d30f1cf87b4a99e4a6c524bf5a17715d636ac6
vamshieega <75155110+vamshieega@users.noreply.github.com>
2021-06-01 00:26:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get("/",function(req,res){
-    res.send("yeah vmashi working"); 
+    res.send("successfully deployed only node.js app in heroku"); 
 })
 
-
 app.listen(process.env.PORT || 5000);
-

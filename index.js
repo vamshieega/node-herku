1975752bf1ef5160da0037a65d2690190c4a5d7c
vamshieega <75155110+vamshieega@users.noreply.github.com>
2022-01-02 15:08:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get("/testRoute",function(req,res){
-    res.send("successfully deployed only node.js app in heroku"); 
+    res.send("successfully deployed only node.js app in heroku testRoute "); 
 })
 
 app.listen(process.env.PORT || 5000);

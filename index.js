761930a45b52e7a79a760f9c20a9448925f7063d
vamshieega <75155110+vamshieega@users.noreply.github.com>
2022-02-25 19:23:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express")
+const cors = require("cors");
 
 const app = express()
 
 app.get("/",function(req,res){
     res.send("Welcome to home"); 
 })
+
+app.use(cors());
+
 
 app.get("/testRoute",function(req,res){
   res.send({
